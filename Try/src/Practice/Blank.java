965e962a5799be1571edc0d373aa819d15e7713e
Admin <Admin@DESKTOP-H8T30GI>
2023-04-21 18:41:55 +0530

    i changed the code

--- Try/src/Practice/Blank.java
+++ Try/src/Practice/Blank.java
@@ -3,18 +3,8 @@ package Practice;
 public class Blank {
 
 	public static void main(String[] args) {
-		int num = 145;
-
-		String temp = Integer.toString(num);
-
-		char a[] = temp.toCharArray();
-
-		int size = a.length;
-
-		for (int i = size - 1; i >= 0; i--) {
-			System.out.print(a[i]);
-
-		}
+	 
+		System.out.println("check git repo");
 	}
 
 }
